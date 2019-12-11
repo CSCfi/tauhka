b95ca179ca1bc66581f74217c710b9e89f44b175
Juhapekka Piiroinen <juhapekka.piiroinen@csc.fi>
2019-12-11 12:22:00 +0200

    Version 0.0.10.

--- tauhka/__init__.py
+++ tauhka/__init__.py
@@ -1,6 +1,6 @@
 __name__ = "tauhka"
 __all__ = ["testcase"]
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 __author__ = "CSC - IT Center for Science Ltd."
 __copyright__ = "Copyright (C) 2019 CSC - IT Center for Science Ltd."
 __license__ = "MIT"
